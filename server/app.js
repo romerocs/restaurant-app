3821b560788e149b0e42baa371a654d6cc55370b
cromero <cromero@richmond.edu>
2018-10-14 18:51:04 -0400

    debugging statiic html issue

--- server/app.js
+++ server/app.js
@@ -9,8 +9,8 @@ var indexRouter = require('./src/routes');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, './src/views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, './src/views'));
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());

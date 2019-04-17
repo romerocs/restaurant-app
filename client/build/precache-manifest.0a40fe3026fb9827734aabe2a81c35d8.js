cc3583f39ca527d332c6382524eea0ff70ff5902
Chris.Romero <chris.romero@ChrisRomeroMacBookPro.local>
2019-04-17 09:51:13 -0400

    pushing compiled assets

---
+++ client/build/precache-manifest.0a40fe3026fb9827734aabe2a81c35d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36597becc8791b0c09ec",
+    "url": "/static/css/main.c9889180.chunk.css"
+  },
+  {
+    "revision": "36597becc8791b0c09ec",
+    "url": "/static/js/main.89506435.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e6167b2635226cd815b2",
+    "url": "/static/js/2.8b511a6c.chunk.js"
+  },
+  {
+    "revision": "f2de9cf44bde5f04d7b75619342e1881",
+    "url": "/index.html"
+  }
+];

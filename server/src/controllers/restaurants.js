2764c0709e7662eed54f79cfb9a2d125a79bd332
Chris.Romero <chris.romero@ChrisRomeroMacBookPro.local>
2019-07-08 11:32:24 -0400

    upgraded sequelize and fixed breaking changes

--- server/src/controllers/restaurants.js
+++ server/src/controllers/restaurants.js
@@ -32,7 +32,7 @@ module.exports = {
         return Restaurants.findOne({
             where: {
                 category: req.params.category,
-                meal: {[sequelize.Op.contains]: [req.params.meal]}
+                meal: {[Symbol.for('sequelize.operator.contains')]: [req.params.meal]}
             },
             order: sequelize.fn('RANDOM')
         })
